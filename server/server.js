2e3a8d272801b51629cc0d837312097164632cac
Kendra Ware <k.ware22@gmail.com>
2018-01-10 17:38:55 -0500

    Add PATCH/todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 
 var {mongoose} = require('./db/mongoose.js');
@@ -25,7 +26,7 @@ app.post('/todos', (req, res) => {
    });
 });
 
-//GET 
+//GET Route
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos})
@@ -49,6 +50,7 @@ app.get('/todos/:id', (req, res) => {
     });
 })
 
+//DELETE Route
 app.delete('/todos/:id', (req, res) => {
     var id = req.params.id;
 
@@ -64,6 +66,33 @@ app.delete('/todos/:id', (req, res) => {
         res.status(400).send();
     })
 });
+
+//UPDATE Route
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+});
+
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
